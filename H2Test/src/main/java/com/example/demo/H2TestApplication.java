fd254fbd84e827bfbbef291ead990331def99d71
win.7 <win.7@win7-PC>
2019-07-10 00:18:17 +0530

    Changes in annotations and web methods

--- H2Test/src/main/java/com/example/demo/H2TestApplication.java
+++ H2Test/src/main/java/com/example/demo/H2TestApplication.java
@@ -1,17 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan({"controller","service"})
+@SpringBootApplication(scanBasePackages={"controller","service"})
 public class H2TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2TestApplication.class, args);
 	}
-
 }
--- H2Test/src/main/java/controller/DbController.java
+++ H2Test/src/main/java/controller/DbController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +50,7 @@ public class DbController {
 	public String putData() {
 		String ret="Table already has the record !";
         try {
-			jdbcTemplate.update("INSERT INTO employee(ID, NAME, AGE) VALUES(1,'Indrashis','27'),(2,'Suman',29),(3,'Govinda',24)");
+			jdbcTemplate.update("INSERT INTO employee(ID, NAME, AGE) VALUES(1,'INDRASHIS','27'),(2,'SUMAN',29),(3,'GOVINDA',24)");
 			ret="Table employee populated successfully !";
 		}
 		catch(Exception e) {
@@ -61,6 +62,20 @@ public class DbController {
 	@RequestMapping(value="/getall",method=RequestMethod.GET)
 	public List<Employee> showAll() {
 		return jdbcTemplate.query("select * from employee", new RowMapper<Employee>() {
+			@Override
+			public Employee mapRow(ResultSet rs, int rownumber) throws SQLException {
+				Employee emp=new Employee();
+				emp.setId(rs.getInt("ID"));
+				emp.setAge(rs.getInt("AGE"));
+				emp.setName(rs.getString("NAME"));
+				
+				return emp;
+			}
+		});
+	}
+	@RequestMapping("/get/{id}")
+	public List<Employee> insertRecord(@PathVariable("id") String id) {
+		return jdbcTemplate.query(("select * from employee where NAME='"+id.toUpperCase()+"'"), new RowMapper<Employee>() {
 			@Override
 			public Employee mapRow(ResultSet rs, int rownumber) throws SQLException {
 				Employee emp=new Employee();
